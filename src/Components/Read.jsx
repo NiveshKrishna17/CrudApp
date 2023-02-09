d0b2dde03e18641f164bebab9282366a8f2b8eea
NiveshKrishna17 <niveshkrishnakamatchi@gmail.com>
2023-02-09 23:19:52 +0530

    Read

--- src/Components/Read.jsx
+++ src/Components/Read.jsx
@@ -1,7 +1,30 @@
-import React from 'react'
+import axios from 'axios';
+import React, { Fragment, useEffect, useState } from 'react'
 
 export default function Read() {
+    const [data, setData]= useState({});
+
+    useEffect(()=>{
+        axios.get(`https://reqres.in/api/users/1`).then((res)=>{
+            setData(res.data.data);
+        })
+    }, []);
+
   return (
+    <Fragment>
     <div>Read</div>
+        <div className='container'>
+            <div className='col-md-12'>
+                <div className='row justify-content-center'>
+                    <h4>First Name is : {data.first_name}</h4>
+                    <h4>Last Name is : {data.last_name}</h4>
+                    <h4>Email Address is : {data.email}</h4>
+                    <h4>AVATAR is : 
+                        <img src={data.avatar} alt="avatar"/>
+                         </h4>
+                </div>
+            </div>
+        </div>
+    </Fragment>
   )
 }
