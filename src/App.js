8ae08d269e3cfef9062f75d83b133b797b3ed3ee
NiveshKrishna17 <niveshkrishnakamatchi@gmail.com>
2023-02-09 22:48:37 +0530

    Create

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Home from './Home';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
--- src/Components/Create.jsx
+++ src/Components/Create.jsx
@@ -1,7 +1,50 @@
-import React from 'react'
+import React, { Fragment } from 'react'
+import { useForm } from 'react-hook-form';
+import axios from 'axios';
+import { toast } from 'react-toastify'
 
-export default function Create() {
-  return (
-    <div>Create</div>
-  )
+const Create = () => {
+    const { register, handleSubmit, formState: { errors }, reset, triggers } = useForm();
+    const onSubmit = (e) => {
+        var obj = {
+            "name": e.userName,
+            "job": e.jobName
+        }
+        axios.post(`https://reqres.in/api/users`, obj).then((res) => {
+            // if (res.status === 201) {
+                toast.success("Created Successfully");
+                reset();
+            // }else {
+            //     toast.error('Something Went Wrong');
+            // }
+        })
+
+
+    }
+    return (
+        <Fragment>
+            <div>Create</div>
+            <div className='container'>
+                <div className='col-md-6'>
+                    <div className='row justify-content-center'>
+                        <form onSubmit={handleSubmit(onSubmit)}>
+                            <input className='form-control' placeholder=' User Name' type="text" name='userName' 
+                            {...register('userName', { required: true })}
+                            />
+                             {errors.userName && <p>User name is required.</p>}
+                            <br />
+                            <input className='form-control' placeholder=' Job Name' type="text" name='jobName' 
+                            {...register('jobName', { required: true })}
+                            />
+                             {errors.jobName && <p>Job name is required.</p>}
+                             <br />
+                             <button className='btn btn-outline-success'>Submit</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </Fragment>
+
+    )
 }
+export default Create;
